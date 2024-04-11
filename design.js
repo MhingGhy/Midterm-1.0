bca12e7e869570b3890994bd8566076f0489636c
MhingGhy <157564984+MhingGhy@users.noreply.github.com>
2024-04-11 14:39:02 +0800

    Update design.js

--- design.js
+++ design.js
@@ -1,1 +1,13 @@
+let text = 'Hello, I am typing...';
+let index = 0;
+let typingElement = document.getElementById('typing');
 
+function typeText() {
+  if (index < text.length) {
+    typingElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeText, 200); // The speed of typing, in milliseconds
+  }
+}
+
+typeText();
